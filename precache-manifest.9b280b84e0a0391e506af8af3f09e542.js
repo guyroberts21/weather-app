7a5a920fa5af139a9fe4abf8707121027d1ca522
guyroberts21 <guyroberts21@outlook.com>
2020-07-16 23:37:00 +0100

    Updates

---
+++ precache-manifest.9b280b84e0a0391e506af8af3f09e542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b348260e5cb716e88c36ca495e19661e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "95cb8f81180fed37bfc3",
+    "url": "/weather-app/static/css/main.a737ca07.chunk.css"
+  },
+  {
+    "revision": "b01c63b6fbb5d21054be",
+    "url": "/weather-app/static/js/2.f31e0058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.f31e0058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95cb8f81180fed37bfc3",
+    "url": "/weather-app/static/js/main.c6cbc16f.chunk.js"
+  },
+  {
+    "revision": "6217e7c726a47d063da4",
+    "url": "/weather-app/static/js/runtime-main.8ce43dfd.js"
+  },
+  {
+    "revision": "8e96146bf58f0806c3b0f0190d5e5898",
+    "url": "/weather-app/static/media/search.8e96146b.svg"
+  }
+]);
